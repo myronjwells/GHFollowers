28e9e068a0d8c17c02be38967f8db3a8e61550c4
Myron Wells <myronjwells@gmail.com>
2023-06-17 20:35:51 -0400

    Initial Commit

---
+++ GHFollowers/FavoritesVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesVC.swift
+//  GHFollowers
+//
+//  Created by Myron Wells on 6/17/23.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Myron Wells on 6/17/23.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
