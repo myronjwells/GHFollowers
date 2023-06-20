4dec44e2f60249f7a507caa6d25f5c9ca4cc4aec
myronjwells <myronjwells@gmail.com>
2023-06-19 22:22:05 -0400

    simple refactoring of the error messages in NetworkManager

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Myron Wells on 6/19/23.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This usrname created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+    
+}
