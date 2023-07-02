71caec09ec71c20e4490ff5b7328275ba6a7d274
myronjwells <myronjwells@gmail.com>
2023-07-02 17:03:41 -0400

    Created a custom view for empty state and implemented it with FollowersListVC

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -26,7 +26,7 @@ class FollowersListVC: UIViewController {
         configureCollectionView()
         getFollowers(username: username, page: page)
         configureDataSource()
-
+        
     }
     
     func configureCollectionView() {
@@ -48,13 +48,20 @@ class FollowersListVC: UIViewController {
     func getFollowers(username: String, page: Int) {
         showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) {[weak self] result in
-           
+            
             guard let self = self else { return }
             self.dismissLoadingView()
             switch result {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
+                
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them 😀."
+                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
+                    return
+                }
                 self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
